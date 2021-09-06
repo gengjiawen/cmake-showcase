c7e69a457a4ccfc6f94a0b72d525eeb2f7f8f012
Jiawen Geng <technicalcute@gmail.com>
2021-09-06 05:34:53 +0000

    print msvc version

--- predefined-macro/main.cpp
+++ predefined-macro/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    auto b = __cplusplus;
-    std::cout << "cpplus: " << b << std::endl;
+  auto b = __cplusplus;
+  std::cout << "cpplus: " << b << std::endl;
+#if defined(_MSC_VER)
+  auto c = _MSC_VER;
+  std::cout << "ms version: " << c << std::endl;
+#endif
 }
